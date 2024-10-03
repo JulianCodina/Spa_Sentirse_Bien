614c4c804d1ec448b0a9934ede8b4b3c7f06ff61
exequielcabrera98 <execabrerasarrio@gmail.com>
2024-10-03 19:06:47 -0300

    modificaciones en app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
+
+import authRoutes from './routes/auth.routes.js'
+import commentRoutes from './routes/comment.routes.js'
+import newsRoutes from './routes/news.routes.js'
+import bookingRoutes from './routes/booking.routes.js'
 
 const app = express()
 
@@ -8,6 +14,15 @@ app.use(cors({
     origin: 'https://spa-sentirse-bien.netlify.app/', 
     credentials: true,
 }))
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api",authRoutes)
+app.use("/api",commentRoutes)
+app.use("/api", newsRoutes)
+app.use("/api", bookingRoutes)
 
 // Ruta principal
 app.get('/', (req, res) => {
