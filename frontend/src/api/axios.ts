40bda46576a996be80c1a5f518ebee1a5a228829
AstridV23 <astridcarla123@gmail.com>
2024-10-06 17:13:15 -0300

    arreglos en las rutas

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const baseURL =
   import.meta.env.NODE_ENV === "production"
-    ? import.meta.env.BACK_URL_PROD
-    : import.meta.env.BACK_URL_LOCAL;
+    ? import.meta.env.VITE_BACK_URL_PROD
+    : import.meta.env.VITE_BACK_URL_LOCAL;
 
 // Definir baseURL dinámicamente según el entorno
 const instance = axios.create({
