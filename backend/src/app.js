27775c0e14c2ad6b2e7385e067326125db28ce44
JulianCodina <depedrojulianismael@gmail.com>
2024-10-06 11:38:43 -0300

    a

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,34 +1,36 @@
-import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
-import authRoutes from './routes/auth.routes.js'
-import commentRoutes from './routes/comment.routes.js'
-import newsRoutes from './routes/news.routes.js'
-import bookingRoutes from './routes/booking.routes.js'
-import pdfRoutes from './routes/pdf.routes.js'
+import authRoutes from "./routes/auth.routes.js";
+import commentRoutes from "./routes/comment.routes.js";
+import newsRoutes from "./routes/news.routes.js";
+import bookingRoutes from "./routes/booking.routes.js";
+import pdfRoutes from "./routes/pdf.routes.js";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    origin: 'https://spa-sentirse-bien.netlify.app', 
+app.use(
+  cors({
+    origin: "http://localhost:3000/api",
     credentials: true,
-}))
+  })
+);
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cookieParser())
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api", authRoutes)
-app.use("/api", commentRoutes)
-app.use("/api", newsRoutes)
-app.use("/api", bookingRoutes)
-app.use("/api", pdfRoutes)
+app.use("/api", authRoutes);
+app.use("/api", commentRoutes);
+app.use("/api", newsRoutes);
+app.use("/api", bookingRoutes);
+app.use("/api", pdfRoutes);
 
 // Ruta principal
-app.get('/', (req, res) => {
-    res.send('¡Hola desde el backend!');
+app.get("/", (req, res) => {
+  res.send("¡Hola desde el backend!");
 });
 
-export default app
+export default app;
